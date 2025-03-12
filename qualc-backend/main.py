36828a9b30fd9b0830549a58a19b1cb40de2d120
ShhTair <tair.shajzada@gmail.com>
2025-03-12 19:27:17 +0500

    com

---
+++ qualc-backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Разрешаем запросы с фронта
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],# Разрешаем фронтенду React
+    allow_credentials=True,
+    allow_methods=["*"], # Разрешаем все методы (GET, POST и т.д.)
+    allow_headers=["*"], # Разрешаем все заголовки
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Qualc API is running!"}
